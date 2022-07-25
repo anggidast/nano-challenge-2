9546aeab244476c700fedf951acffe7866bedb22
anggidast <adastariana@gmail.com>
2022-07-25 10:02:28 +0700

    init

---
+++ NanoChallenge2/NanoChallenge2App.swift
@@ -0,0 +1,17 @@
+//
+//  NanoChallenge2App.swift
+//  NanoChallenge2
+//
+//  Created by feedloop on 25/07/22.
+//
+
+import SwiftUI
+
+@main
+struct NanoChallenge2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
